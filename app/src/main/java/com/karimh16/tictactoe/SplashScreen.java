faf7a10811fbab130ea2cc33940c80dc78f498ed
karim hemina <karimhemina16@gmail.com>
2019-03-17 18:36:47 +0100

    add splashScreen

---
+++ app/src/main/java/com/karimh16/tictactoe/SplashScreen.java
@@ -0,0 +1,43 @@
+package com.karimh16.tictactoe;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_splash_screen);
+
+        Thread thread = new Thread(){
+            @Override
+            public void run() {
+                try{
+                    sleep(1000);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }finally {
+                    Intent intent = new Intent(SplashScreen.this, TwoPlayers.class);
+                    startActivity(intent);
+                }
+            }
+        };
+
+        thread.start();
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+}
